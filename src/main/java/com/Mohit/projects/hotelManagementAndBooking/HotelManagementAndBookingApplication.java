f87f2b358fb14525ead9b290a8cbc09a2d10996a
K Mohit <itzmohit96@gmail.com>
2026-01-17 03:27:43 +0530

    initial_setup

---
+++ src/main/java/com/Mohit/projects/hotelManagementAndBooking/HotelManagementAndBookingApplication.java
@@ -0,0 +1,13 @@
+package com.Mohit.projects.hotelManagementAndBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementAndBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementAndBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Mohit/projects/hotelManagementAndBooking/HotelManagementAndBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mohit.projects.hotelManagementAndBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementAndBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

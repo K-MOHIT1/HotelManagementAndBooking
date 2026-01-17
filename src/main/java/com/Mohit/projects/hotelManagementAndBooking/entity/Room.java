ba35851ebb63160b8a159ea3fca1eb129eaf519a
K Mohit <itzmohit96@gmail.com>
2026-01-17 19:05:43 +0530

    Entities_added

---
+++ src/main/java/com/Mohit/projects/hotelManagementAndBooking/entity/Room.java
@@ -0,0 +1,58 @@
+package com.Mohit.projects.hotelManagementAndBooking.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "room")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "hotel_id",nullable = false)
+    private Hotel hotel;
+
+    @Column(nullable = false)
+    private String Type;
+
+    @Column(nullable = false,precision = 10, scale = 2)
+    private BigDecimal basePrice;
+
+    @Column(columnDefinition = "TEXT[]")
+    private String[] photos;
+
+    @Column(columnDefinition = "TEXT[]")
+    private String[] amenities;
+
+    @Column(nullable = false)
+    private Integer totalCount;
+
+    @Column(nullable = false)
+    private Integer capacity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+
+
+}
